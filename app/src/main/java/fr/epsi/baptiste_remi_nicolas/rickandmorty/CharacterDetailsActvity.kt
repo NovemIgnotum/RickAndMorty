bf5c3e30db07f4d3fda999998e7de6f088bf22b6
RemiPontello <remi.pontello@ecoles-epsi.net>
2024-06-19 09:19:35 +0200

    toolbar

--- app/src/main/java/fr/epsi/baptiste_remi_nicolas/rickandmorty/CharacterDetailsActvity.kt
+++ app/src/main/java/fr/epsi/baptiste_remi_nicolas/rickandmorty/CharacterDetailsActvity.kt
@@ -1,12 +1,17 @@
 package fr.epsi.baptiste_remi_nicolas.rickandmorty
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
+import android.widget.ScrollView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
-import network.RickAndMortyApiService
+import androidx.appcompat.widget.Toolbar
 import coil.load
+import network.RickAndMortyApiService
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -17,6 +22,19 @@ class CharacterDetailsActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.character_details)
 
+        val toolbar: Toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Enable the up button
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        // Handle the back button press
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                finish()
+            }
+        })
+
         val characterId = intent.getIntExtra("character_id", -1)
         if (characterId != -1) {
             fetchCharacterDetails(characterId)
@@ -25,40 +43,65 @@ class CharacterDetailsActivity : AppCompatActivity() {
         }
     }
 
-    private fun fetchCharacterDetails(characterId: Int) {
-        RickAndMortyApiService.api.getCharacter(characterId).enqueue(object : Callback<Character> {
-            override fun onResponse(call: Call<Character>, response: Response<Character>) {
-                if (response.isSuccessful) {
-                    val character = response.body()
-                    if (character != null) {
-                        updateUI(character)
-                    } else {
-                        showError("Impossible de récupérer les détails du personnage.")
-                    }
-                } else {
-                    showError("Impossible de récupérer les détails du personnage.")
-                }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                onBackPressedDispatcher.onBackPressed()
+                true
             }
-
-            override fun onFailure(call: Call<Character>, t: Throwable) {
-                showError("Impossible de récupérer les détails du personnage.")
-            }
-        })
-    }
-
-    private fun updateUI(character: Character) {
-        findViewById<TextView>(R.id.characterName).text = character.name
-        findViewById<TextView>(R.id.characterSpecies).text = character.species
-        findViewById<TextView>(R.id.characterStatus).text = character.status
-        findViewById<TextView>(R.id.characterOrigin).text = character.origin.name
-        findViewById<TextView>(R.id.characterLocation).text = character.location.name
-
-        val characterImageView = findViewById<ImageView>(R.id.characterImage)
-        characterImageView.load(character.image) {
+            else -> super.onOptionsItemSelected(item)
         }
     }
 
-    private fun showError(errorMessage: String) {
-        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
-    }
-}
+                private fun fetchCharacterDetails(characterId: Int) {
+                    RickAndMortyApiService.api.getCharacter(characterId)
+                        .enqueue(object : Callback<Character> {
+                            override fun onResponse(
+                                call: Call<Character>,
+                                response: Response<Character>
+                            ) {
+                                if (response.isSuccessful) {
+                                    val character = response.body()
+                                    if (character != null) {
+                                        updateUI(character)
+                                    } else {
+                                        showError("Impossible de récupérer les détails du personnage.")
+                                    }
+                                } else {
+                                    showError("Impossible de récupérer les détails du personnage.")
+                                }
+                            }
+
+                            override fun onFailure(call: Call<Character>, t: Throwable) {
+                                showError("Impossible de récupérer les détails du personnage.")
+                            }
+                        })
+                }
+
+                private fun updateUI(character: Character) {
+                    findViewById<TextView>(R.id.characterName).text = character.name
+                    findViewById<TextView>(R.id.characterSpecies).text = character.species
+                    findViewById<TextView>(R.id.characterStatus).text = character.status
+                    findViewById<TextView>(R.id.characterOrigin).text = character.origin.name
+                    findViewById<TextView>(R.id.characterLocation).text = character.location.name
+
+                    val characterImageView = findViewById<ImageView>(R.id.characterImage)
+                    characterImageView.load(character.image) {
+                    }
+
+                    supportActionBar?.title = character.name
+                }
+
+                private fun showError(errorMessage: String) {
+                    Toast.makeText(this@CharacterDetailsActivity, errorMessage, Toast.LENGTH_SHORT)
+                        .show()
+                    findViewById<ScrollView>(R.id.characters_rv).visibility = View.GONE
+                    findViewById<TextView>(R.id.errorMessage).apply {
+                        visibility = View.VISIBLE
+                        text = errorMessage
+                    }
+
+
+                }
+
+            }
